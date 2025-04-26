25cc7bac28657e4a3c2bf45c6c42c3d8c5f6f997
Dereje <dereje2012k@gmail.com>
2025-04-26 20:36:02 +0300

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function (texts) {
+  const textsArray = texts.split(" ");
+  let textReversed = "";
+  let result = [];
 
+  for (const text of textsArray) {
+    for (let i = 1; i <= text.length; i++) {
+      textReversed += text.at(-i);
+    }
+    result.unshift(textReversed);
+    textReversed = "";
+  }
+
+  return result.join(" ");
 };
 
 // Do not edit below this line
