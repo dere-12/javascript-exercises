cbd0437a6882e0be8623895aa933bef42ab6462f
Dereje <dereje2012k@gmail.com>
2025-04-28 11:55:58 +0300

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...otherPara) {
+  const filtered = array.filter((item) => {
+    for (let i = 0; i < otherPara.length; i++) {
+      if (item === otherPara[i]) {
+        return false;
+      }
+    }
+    return item;
+  });
+
+  return filtered;
 };
 
 // Do not edit below this line
