96ef9df70aa1e190f30ff70e536ce3d1e2243c14
Dereje <dereje2012k@gmail.com>
2025-05-10 13:16:10 +0300

    Complete exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
+const findTheOldest = function (people) {
+  const sorted = people.sort((a, b) => {
+    if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  return sorted.pop();
 };
+
+console.log(findTheOldest(people));
+
+// To be Continued.
 
 // Do not edit below this line
 module.exports = findTheOldest;
