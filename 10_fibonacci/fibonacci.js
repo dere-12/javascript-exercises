2900bba67f2daf12a018d624592c1277df6c5ff9
Dereje <dereje2012k@gmail.com>
2025-05-10 09:33:04 +0300

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = parseInt(n);
+  let fibonacci = [1];
+  for (let i = 1; i <= n; i++) {
+    let lastElement = fibonacci[fibonacci.length - 1];
+    let beforeLastElement = fibonacci[fibonacci.length - 2];
+    if (beforeLastElement === undefined) {
+      fibonacci.push(1);
+    } else {
+      fibonacci.push(lastElement + beforeLastElement);
+    }
+  }
 
+  if (n === 0) {
+    return 0;
+  } else if (n < 0) {
+    return "OOPS";
+  } else {
+    return fibonacci[n - 1];
+  }
 };
 
 // Do not edit below this line
